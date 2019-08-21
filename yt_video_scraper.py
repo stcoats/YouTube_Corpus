e3fb7ed3babfd90ed60756562e4177e6cd2dbb9b
stcoats <32770789+stcoats@users.noreply.github.com>
2019-08-21 14:24:50 +0300

    Create yt_video_scraper.py

---
+++ yt_video_scraper.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import subprocess
+
+xxx=[]
+dnew_p = pd.DataFrame()
+for i,w in dn1.iterrows():
+    if not w["channel_id"] in old_channels["channel_id"]:
+            result = subprocess.run(["youtube-dl",
+            "https://www.youtube.com/channel/"+w["channel_id"],
+            "--skip-download",
+            "--get-title",
+            "--get-id",
+            "--verbose"],
+             stdout=subprocess.PIPE)
+            proc = result.stdout
+            if proc:
+                groups = proc.split(b'\n')
+                for x,y in zip(groups[1::3], groups[2::3]):
+                    if x not in xxx:
+                            dnew_p= dnew_p.append([[w["state"],w["search_term"],
+                                                  w["channel_title"],w["channel_id"],
+                                                x.decode('utf-8'),y.decode('utf-8')]])
+                            result1 = subprocess.run(["youtube-dl",
+                                "--skip-download",
+                                "--write-auto-sub",
+                                "--sub-lang",
+                                "en",
+                                "https://youtu.be/"+y.decode('utf-8'),
+                                "-o",
+                                "/home/cloud-user/taito_wrk/DONOTREMOVE/dialects/government_videos/%s"% w["state"]+"/%s"% w["channel_title"]+"/%(title)s.%(ext)s" ],
+                                     stdout=subprocess.PIPE)
+                            proc1 = result.stdout
+                            xxx.extend(x)
+
+dnew_p.reset_index(drop=True)
+dnew_p.columns = ["state","search_term","channel_name","channel_id","video_title","video_id"]
