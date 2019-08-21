72ed2423f8cbd55686e8e5e53e120b22528610c3
stcoats <32770789+stcoats@users.noreply.github.com>
2019-08-21 15:19:13 +0300

    Update yt_video_scraper.py

--- yt_video_scraper.py
+++ yt_video_scraper.py
@@ -36,3 +36,46 @@ for i,w in dn1.iterrows():
 
 dnew_p.reset_index(drop=True)
 dnew_p.columns = ["state","search_term","channel_name","channel_id","video_title","video_id"]
+
+import re
+import pandas as pd
+import html5lib
+from bs4 import BeautifulSoup
+import webvtt
+
+def remove_adjacent(nums):
+     return [a for a,b in zip(nums, nums[1:]+[not nums[-1]]) if a != b]
+    
+def get_sec(time_str):
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + float(s)
+
+dnp_good=pd.DataFrame()
+for i,x in dnew_p.iterrows():
+    vid = "/path/to/storage%s/%s/%s"%(x["state"],x["channel_name"],x["video_title"])+".en.vtt"
+    vid1 = re.sub("\:"," -",vid)
+    try:
+        stripped1=[]
+        time = []
+        time1 = []
+        captions = webvtt.read(vid1)
+        for caption in captions:
+            stripped = caption.text.replace('\n', "\n")
+            stripped1.append(stripped.split("\n"))
+            gg = [x for y in stripped1 for x in y if x != " "]
+            gg1 = " ".join(remove_adjacent(gg))
+            caption1 = re.sub("\n","",caption.text)
+            #print(caption1)
+            if not re.search("^\s*$|^\s*\[Applause\]$|^\s*\[Music\]$|^\s*\[Laughter\]$",caption1):
+                time.append(get_sec(caption.end)-get_sec(caption.start))
+                
+            time1 = sum(time)
+            #print(time1)
+
+        dnp_good=dnp_good.append([[x["state"],x["search_term"],x["channel_name"],x["channel_id"],
+                           x["video_title"],x["video_id"],gg1,time1]])
+    except:
+        continue
+        
+dnp_good.columns = ["state","search_term","channel_name","channel_id","video_title",
+              "video_id","text","speech_duration"]
